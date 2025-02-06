fe3c8cac7af6190913ddd71fadf56325538b5447
Ravinder Sirohi <ravinder@hovergen.com>
2025-02-06 08:51:08 +0000

    Only Fast API

--- python-chat-open-ai/main.py
+++ python-chat-open-ai/main.py
@@ -2,12 +2,12 @@
 from fastapi import FastAPI
 # from pydantic import BaseModel
 # import openai
-import os
-import aiohttp
+# import os
+# import aiohttp
 
 app = FastAPI()
 
-OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+# OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 
 # Set your OpenAI API key
 # openai.api_key = OPENAI_API_KEY
